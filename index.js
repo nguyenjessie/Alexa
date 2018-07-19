45afb83b36e7881b23f81d8386fdf0c2d740890c
Jessie Nguyen <ngjessie@amazon.com>
2018-07-19 13:41:50 -0700

    finish basic code

---
+++ index.js
@@ -0,0 +1,131 @@
+// LAMBDA HANDLER
+
+// Creates a Lambda handler function to route all inbound request to your skill
+// Creates an SDK Skill instance configured with the request handlers we create
+'use strict';
+
+const Alexa = require('ask-sdk-core');
+// use 'ask-sdk' if standard SDK module is installed
+
+// Code for the handlers here
+
+let skill;
+
+exports.handler = async function (event, context) {
+  console.log(`REQUEST++++${JSON.stringify(event)}`);
+  if (!skill) {
+    skill = Alexa.SkillBuilders.custom()
+      .addRequestHandlers(
+        LaunchRequestHandler,
+        HelloWorldIntentHandler,
+        HelpIntentHandler,
+        CancelAndStopIntentHandler,
+        SessionEndedRequestHandler,
+      )
+      .addErrorHandlers(ErrorHandler)
+      .create();
+  }
+
+  return skill.invoke(event,context);
+}
+
+
+
+// HANDLERS
+
+// Be invoked when the skill is invoked without a specific intent
+const LaunchRequestHandler = {
+    // Returns true if LaunchRequest
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
+    },
+    handle(handlerInput) {
+        const speechText = 'Welcome to the Alexa Skills Kit, you can say hello!';
+
+        return handlerInput.responseBuilder
+            .speak(speechText)
+            .reprompt(speechText)
+            .withSimpleCard('Hello World', speechText)
+            .getResponse();
+    }
+};
+
+// Be invoked when the skill receives the HelloWorldIntent
+const HelloWorldIntentHandler = {
+    // Returns true if IntentRequest
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+            && handlerInput.requestEnvelope.request.intent.name === 'HelloWorldIntent';
+    },
+    handle(handlerInput) {
+        const speechText = 'Hello World!';
+
+        return handlerInput.responseBuilder
+            .speak(speechText)
+            .withSimpleCard('Hello World', speechText)
+            .getResponse();
+    }
+};
+
+// Be invoked when the skill receives the AMAZON.HelpIntent
+const HelpIntentHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+            && handlerInput.requestEnvelope.request.intent.name === 'AMAZON.HelpIntent';
+    },
+    handle(handlerInput) {
+        const speechText = 'You can say hello to me!';
+
+        return handlerInput.responseBuilder
+            .speak(speechText)
+            .reprompt(speechText)
+            .withSimpleCard('Hello World', speechText)
+            .getResponse();
+    }
+};
+
+// Be invoked when the skill receives Amazon.CancelIntent and Amazon.StopIntent
+// Both responses are the same, so both handled in one block
+const CancelAndStopIntentHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+            && (handlerInput.requestEnvelope.request.intent.name === 'AMAZON.CancelIntent'
+                || handlerInput.requestEnvelope.request.intent.name === 'AMAZON.StopIntent');
+    },
+    handle(handlerInput) {
+        const speechText = 'Goodbye!';
+
+        return handlerInput.responseBuilder
+            .speak(speechText)
+            .withSimpleCard('Hello World', speechText)
+            .getResponse();
+    }
+};
+
+// SessionEndedRequest Handler
+const SessionEndedRequestHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'SessionEndedRequest';
+    },
+    handle(handlerInput) {
+        // Any cleanup logic goes here
+        return handlerInput.responseBuilder.getResponse();
+    }
+};
+
+// Error handler is a good place to inject your error handling logic such as unhandled request,
+// api service time out, etc. The following sample adds a catch all error handler to your skill
+// to ensure skill returns a meaningful message in case of all errors.
+const ErrorHandler = {
+    canHandle() {
+      return true;
+    },
+    handle(handlerInput, error) {
+      console.log(`Error handled: ${error.message}`);
+
+      return handlerInput.responseBuilder
+        .speak('Sorry, I can\'t understand the command. Please say again.')
+        .reprompt('Sorry, I can\'t understand the command. Please say again.')
+        .getResponse();
+    },
+};
